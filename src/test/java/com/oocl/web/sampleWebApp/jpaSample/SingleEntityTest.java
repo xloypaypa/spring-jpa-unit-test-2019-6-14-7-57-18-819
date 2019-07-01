040c84cd1cdbbfa8434a7e769beacaf1e3e9b9ec
xsu <xsu@thoughtworks.com>
2019-07-01 15:54:19 +0800

    [Xin Su] test: should be able to create and fetch entity.

---
+++ src/test/java/com/oocl/web/sampleWebApp/jpaSample/SingleEntityTest.java
@@ -0,0 +1,43 @@
+package com.oocl.web.sampleWebApp.jpaSample;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest()
+@AutoConfigureMockMvc
+public class SingleEntityTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void should_be_able_to_create_entity() throws Exception {
+        ResultActions perform = this.mockMvc.perform(put("/singleEntity")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{\"id\":3,\"name\":\"my name\"}"));
+
+        perform.andDo(print()).andExpect(status().isCreated());
+    }
+
+    @Test
+    public void should_be_able_to_fetch_entity() throws Exception {
+        ResultActions perform = this.mockMvc.perform(get("/singleEntity"));
+
+        perform.andDo(print()).andExpect(status().isOk()).andExpect(content().string(is("[]")));
+    }
+}
